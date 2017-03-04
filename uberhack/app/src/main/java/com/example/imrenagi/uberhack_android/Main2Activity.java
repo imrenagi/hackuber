764d64ba18cded2e89af51f173560395a9d55170
Imre Nagi <imre.nagi2812@gmail.com>
2017-03-04 01:44:37 -0800

    Add video

--- uberhack/app/src/main/java/com/example/imrenagi/uberhack_android/Main2Activity.java
+++ uberhack/app/src/main/java/com/example/imrenagi/uberhack_android/Main2Activity.java
@@ -1,14 +1,18 @@
 package com.example.imrenagi.uberhack_android;
 
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 
 import java.io.IOException;
@@ -19,13 +23,52 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        final Button closeButton = (Button) findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlertDialog();
+            }
+        });
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         VideoView videoView = (VideoView) findViewById(R.id.videoView);
-
-        videoView.setVideoPath("http://www.jpl.nasa.gov/videos/wise/20120608/wise20120608-1280.m4v");
+        videoView.setVideoPath("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
         videoView.start();
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                closeButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setCancelable(false);
+        dialog.setTitle("Congrats! You got bonus points!");
+        dialog.setMessage("Do you want to keep this point to your account?");
+        dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                System.out.println("Store it");
+                dialog.dismiss();
+                finish();
+            }
+        }).setNegativeButton("No ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println("fuck it");
+                dialog.dismiss();
+                finish();
+            }
+        });
+        final AlertDialog alert = dialog.create();
+        alert.show();
     }
 
 }
