758d23e54e163136a50715fac2c5d0bddcc0ea4e
Amazingandyyy <amazingandyyy@gmail.com>
2017-03-03 22:51:14 -0800

    add twilio and  hide env and deploy to heroku

--- node-server/index.js
+++ node-server/index.js
@@ -1,10 +1,32 @@
+require('dotenv').config()
+
 import express from 'express';
 const app = express();
+const twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 app.get('/', (req, res)=>res.send('use endpoint/api'))
 
 app.get('/api', (req, res)=>{
     res.send('API works')
 })
+
+app.get('/api/sms', (req, res)=>{
+    const receiver_num = req.query.number;
+    const receiver_name = req.query.name;
+    twilio.sendMessage({
+        to: receiver_num,
+        from: process.env.TWILIO_NUMBER,
+        body: `Hi ${receiver_name}`
+    }, function(err, good){
+        if (err) {
+            console.log('err when send twilio SMS: ', err);
+            return res.status(500).send('fail to send')
+        }
+        // successful
+        console.log(`send message to ${receiver_num}`)
+        res.status(200).send('message sent')
+    })
+})
+
 const PORT = 8001;
 app.listen(process.env.PORT||PORT, err => console.log(err || `->Listening on ${process.env.PORT} || PORT`));
