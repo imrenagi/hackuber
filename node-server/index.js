ac359806486553d53e8c955ddd33f49c0a4b51a9
Amazingandyyy <amazingandyyy@gmail.com>
2017-03-03 22:38:34 -0800

    add PORT as env

--- node-server/index.js
+++ node-server/index.js
@@ -6,5 +6,5 @@ app.get('/', (req, res)=>res.send('use endpoint/api'))
 app.get('/api', (req, res)=>{
     res.send('API works')
 })
-
-app.listen(8001, err => console.log(err || `->Listening on 8001`));
+const PORT = 8001;
+app.listen(process.env.PORT||PORT, err => console.log(err || `->Listening on ${process.env.PORT} || PORT`));
